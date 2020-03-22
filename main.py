da9be56f75c85435c7c70f1980f27a18f6ee81b3
Nicky Goh <nicky.goh@adgeek.com.tw>
2020-03-22 15:20:32 +0800

    Using flask as sample app

--- main.py
+++ main.py
@@ -1,29 +1,9 @@
-from device_detector import DeviceDetector
-import json
+import flask
 
-def lambda_handler(event, context):
-    ua = event['ua']
-    p = DeviceDetector(ua).parse()
+app = flask.Flask(__name__)
 
-    res = []
-
-    device = str(p.all_details.get('device', 'None'))
-    os = str(p.all_details.get('os', 'None'))
-    client = str(p.all_details.get('client', 'None'))
-
-    res.append(device)
-    res.append(os)
-    res.append(client)
-
-    response = filter(lambda x: x != 'None', res)
-    response = ",".join(response)
-
-    return {
-        'statusCode': 200,
-        'body': eval(json.dumps(response))
-    }
-#
-# event = { "ua" : "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0" }
-#
-# print(lambda_handler(event))
+@app.route("/")
+def index():
+    text = "Hello World"
+    return text
 
